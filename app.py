9ee95bd38b17e8c3a3c1f9e8b42a97220be868cf
SoMin <thals0107@naver.com>
2023-02-07 19:23:36 +0900

    feat: Add a method to save data

--- app.py
+++ app.py
@@ -12,5 +12,42 @@ db = client.gofestival
 def home():
   return render_template('detail.html')
 
+@app.route('/savedata', methods=["POST"])
+def data_post():
+  data = request.form
+  print(data)
+  for i in data:
+    contentid = i['contentid']
+    add1 = i['add1']
+    add2 = i['add2']
+    eventstartdate = i['eventstartdate']
+    eventenddate = i['eventenddate']
+    firstimage= i['firstimage']
+    firstimage2= i['firstimage2']
+    mapx = i['mapx']
+    mapy = i['mapy']
+    modifiedtime= i['modifiedtime']
+    tel= i['tel']
+    title= i['title']
+  
+    doc = {
+      contentid,
+      add1,
+      add2,
+      eventstartdate,
+      eventenddate,
+      firstimage,
+      firstimage2,
+      mapx,
+      mapy,
+      modifiedtime,
+      tel,
+      title,
+    }
+
+    db.detail.insert_one(doc)
+  
+  return jsonify({'msg': '!!'})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=4000, debug=True)
