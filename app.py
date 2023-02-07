54685e6c6b3a29c859c3eb2ba6bd15bdd1166eac
SoMin <thals0107@naver.com>
2023-02-07 19:22:35 +0900

    feat: Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+import certifi
+ca = certifi.where()
+client = MongoClient('mongodb+srv://test:thals@cluster0.kbk9mgh.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.gofestival
+
+
+@app.route('/')
+def home():
+  return render_template('detail.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=4000, debug=True)
